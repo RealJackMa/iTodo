b73074f4137e6693c41b6e584dbbad70884f252c
CelestialEmperor <mapeiyao@gmail.com>
2020-09-14 04:14:15 +0100

    Built the MVP App with CRD actions.

--- app/src/main/java/com/example/itodo/MainActivity.java
+++ app/src/main/java/com/example/itodo/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     Button addButton;
     EditText editTextItem;
     RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +37,69 @@ public class MainActivity extends AppCompatActivity {
         editTextItem = findViewById(R.id.editTextItem);
         rvItems = findViewById(R.id.rvItems);
          
-        items = new ArrayList<>();
+        loadItems();
         items.add("Submit CodePath Application.");
         items.add("Submit CodePath Prework.");
-        items.add("Update Linkedin Profile Picture.");
 
-        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
+            @Override
+            public void onItemLongClicked(int position) {
+
+                // Delete the item from the model.
+                items.remove(position);
+                // Notify the adapater at which position we deleted the item.
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item was deleted!", Toast.LENGTH_SHORT).show();
+
+                saveItems();
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
 
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = editTextItem.getText().toString();
+
+                // Add the item to the model.
+                items.add(todoItem);
+
+                // Notify the adapter that an item has been added.
+                itemsAdapter.notifyItemInserted(items.size()-1);
+
+                // Clear editText input.
+                editTextItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item was added!", Toast.LENGTH_SHORT).show();
+
+                saveItems();
+            }
+        });
     }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // This function will load files by reading every line of the data file.
+    private void loadItems(){
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items!", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    // This function will save items by writing into the data file.
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(),items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items!", e);
+        }
+    }
+
 }
