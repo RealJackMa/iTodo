c567ac2cdd98b4b6dfb630f09555022b370dd581
CelestialEmperor <mapeiyao@gmail.com>
2020-09-15 23:59:26 +0100

    Planning.

--- app/src/main/java/com/example/itodo/EditActivity.java
+++ app/src/main/java/com/example/itodo/EditActivity.java
@@ -3,6 +3,7 @@ package com.example.itodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +23,21 @@ public class EditActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Edit Item");
 
         etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // The button is clicked when the user is done editing.
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create an intent which will contain the user's results.
+
+                // Pass the results.
+
+                // Set the result of the intent.
+
+                // Close the editing screen and go back.
+
+            }
+        });
     }
 }
